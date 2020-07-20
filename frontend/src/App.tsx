66e7e7ff94a70519e0d689d2935d3276f653c4ed
sonylomo <49971500+sonylomo@users.noreply.github.com>
2020-07-20 20:26:51 +0300

    added structural changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,31 @@
 import React from 'react';
-import Signup from './components/SignUp';
+import Signup from 'components/SignUp';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from "react-router-dom";
 import './App.css';
+
+
+const AppRoutes = () => {
+  return (
+    <Switch>
+      <Route path="/">
+        <Signup />
+      </Route>
+     
+    </Switch>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <Signup />
-    </div>
+    <Router>
+      <div className="App">
+        <AppRoutes />
+      </div>
+    </Router>
   );
 }
 
